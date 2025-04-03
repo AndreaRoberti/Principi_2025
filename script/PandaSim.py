e1fbc553c8ec51740c41760d9dd33d5190630928
AndreaRoberti <andrea_kid90@yahoo.it>
2025-04-03 18:12:47 +0200

    [ADD] PandaSim

--- script/PandaSim.py
+++ script/PandaSim.py
@@ -13,33 +13,6 @@ panda = PandaRobot(client, "Panda")
 
 panda.startSimulation()
 
-
-
-# Parameters
-T_total = 10   # Total trajectory duration
-t_acc = 2      # Acceleration time
-t_flat = 5     # Constant velocity time
-t_dec = 3      # Deceleration time
-v_max = 2      # Maximum velocity
-
-# Time vector
-t = np.linspace(0, T_total, 1000)
-
-# Compute velocity trajectory
-velocity = panda.trapezoidal_trajectory(t, t_acc, t_flat, t_dec, v_max)
-
-# Plotting
-# plt.figure(figsize=(8, 4))
-# plt.plot(t, velocity, label="Trapezoidal Velocity Profile", color="b")
-# plt.xlabel("Time [s]")
-# plt.ylabel("Velocity [m/s]")
-# plt.title("Trapezoidal Trajectory")
-# plt.legend()
-# plt.grid()
-# plt.show()
-
-
-
 while (t := panda.simulationTime()) < 5:
     print(f'Simulation time: {t:.2f} [s]')
 
